5a68b0cd2808ddcc3b97daeb0e0188191323f1b1
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-05-15 11:59:10 +0400

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    jsxSingleQuote: false,
+    printWidth: 80,
+    useTabs: false,
+    tabWidth: 2,
+    endOfLine: 'lf',
+}
